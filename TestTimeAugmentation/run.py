adc561e9aa8867d6b75ed2b4c90ff506dd68f975
Inoh Francis Monton <37641302+inohmonton99@users.noreply.github.com>
2021-07-17 01:31:28 +0800

    added separate directories for model input paths
    this way dataset files won't conflict with model paths.

--- TestTimeAugmentation/run.py
+++ TestTimeAugmentation/run.py
@@ -17,22 +17,22 @@ def main(args):
     models_list = args.models.split(",")
     print("Models to be run: ", models_list)
     if 'mask_rcnn' in models_list:
-        maskRcnn = testTimeAugmentation.MaskRCNNPred('/mnt/src/mask_rcnn_coco.h5', '/mnt/src/coco.names')
+        maskRcnn = testTimeAugmentation.MaskRCNNPred('/mnt/model/mask_rcnn_coco.h5', '/mnt/model/coco.names')
         listModels.append(maskRcnn)
     if 'retinanet' in models_list:
-        retinaResnet50 = testTimeAugmentation.RetinaNetResnet50Pred('/mnt/src/resnet50_coco_best_v2.1.0.h5', '/mnt/src/coco.csv')
+        retinaResnet50 = testTimeAugmentation.RetinaNetResnet50Pred('/mnt/model/resnet50_coco_best_v2.1.0.h5', '/mnt/model/coco.csv')
         listModels.append(retinaResnet50)
     if 'yolo_darknet' in models_list:
-        yoloDarknet = testTimeAugmentation.DarknetYoloPred('/mnt/src/yolov3.weights', '/mnt/src/coco.names','/mnt/src/yolov3.cfg')
+        yoloDarknet = testTimeAugmentation.DarknetYoloPred('/mnt/model/yolov3.weights', '/mnt/src/coco.names','/mnt/model/yolov3.cfg')
         listModels.append(yoloDarknet)
     if 'ssd_resnet' in models_list:
-        ssdResnet = testTimeAugmentation.MXnetSSD512Pred('/mnt/src/ssd_512_resnet50_v1_voc-9c8b225a.params', '/mnt/src/classesMXnet.txt')
+        ssdResnet = testTimeAugmentation.MXnetSSD512Pred('/mnt/model/ssd_512_resnet50_v1_voc-9c8b225a.params', '/mnt/model/classesMXnet.txt')
         listModels.append(ssdResnet)
     if 'faster_resnet' in models_list:
-        fasterResnet = testTimeAugmentation.MXnetFasterRCNNPred('/mnt/src/faster_rcnn_resnet50_v1b_voc-447328d8.params', '/mnt/src/classesMXnet.txt')
+        fasterResnet = testTimeAugmentation.MXnetFasterRCNNPred('/mnt/model/faster_rcnn_resnet50_v1b_voc-447328d8.params', '/mnt/model/classesMXnet.txt')
         listModels.append(fasterResnet)
     if 'tfod' in models_list:
-        tfodModel = testTimeAugmentation.TFODPred('/mnt/output/frozen_inference_graph.pb', 'classes.csv')
+        tfodModel = testTimeAugmentation.TFODPred('/mnt/model/frozen_inference_graph.pb', '/mnt/model/classes.csv')
         listModels.append(tfodModel)
         
 #     listaModels = [retinaResnet50, maskRcnn]
