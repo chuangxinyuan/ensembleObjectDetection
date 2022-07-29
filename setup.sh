8f1bd04d27764842b476fe82cc6d0e9ddf81ab38
shaiic <niuhx@shaiic.com>
2022-07-29 09:46:59 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ pip install ./Mask_RCNN/ --user
 wget https://www.dropbox.com/sh/n21kckhsi200b52/AABxspis34aAZiMUp_cQ6RYFa?dl=1 -O weights.zip
 unzip weights.zip -d /mnt/src/
 ls
-pip install -r requirements.txt
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
