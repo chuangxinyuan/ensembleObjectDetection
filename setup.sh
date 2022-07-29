6e31776381f1dd7a61c729ae9ddb8217e9b62e0b
shaiic <niuhx@shaiic.com>
2022-07-29 15:34:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ git clone https://github.com/onepanelio/Mask_RCNN
 pip install ./Mask_RCNN/ --user
 wget https://shaiicpublic.blob.core.chinacloudapi.cn/onepanel/weights.zip
 unzip weights.zip -d /mnt/src/
+wget https://shaiicpublic.blob.core.chinacloudapi.cn/onepanel/frozen_inference_graph.pb
+wget https://shaiicpublic.blob.core.chinacloudapi.cn/onepanel/classes.csv
 ls
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
