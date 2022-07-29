03d3d5154966b67a408cc3700367db7c00f812cd
shaiic <niuhx@shaiic.com>
2022-07-29 09:58:27 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ git clone https://github.com/fizyr/keras-retinanet
 pip install ./keras-retinanet/ --user
 git clone https://github.com/onepanelio/Mask_RCNN
 pip install ./Mask_RCNN/ --user
-wget https://www.dropbox.com/sh/n21kckhsi200b52/AABxspis34aAZiMUp_cQ6RYFa?dl=1 -O weights.zip
+wget https://shaiicpublic.blob.core.chinacloudapi.cn/onepanel/weights.zip
 unzip weights.zip -d /mnt/src/
 ls
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
