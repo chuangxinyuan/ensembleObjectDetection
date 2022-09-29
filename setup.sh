d962afb0b8bcba3afb4dd3d38a2065800c12f874
shaiic <niuhx@shaiic.com>
2022-09-29 13:39:36 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ git clone https://github.com/fizyr/keras-retinanet
 pip install ./keras-retinanet/ --user
 git clone https://github.com/onepanelio/Mask_RCNN
 pip install ./Mask_RCNN/ --user
-wget https://shaiicpublic.blob.core.chinacloudapi.cn/onepanel/weights_part.zip
+wget https://shaiicpublic.blob.core.chinacloudapi.cn/onepanel/weights.zip
 unzip weights.zip -d /mnt/src/
 wget https://shaiicpublic.blob.core.chinacloudapi.cn/onepanel/frozen_inference_graph.pb
 wget https://shaiicpublic.blob.core.chinacloudapi.cn/onepanel/classes.csv
